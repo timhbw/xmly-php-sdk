5f41d7240556f03aaf08e74cdb078139bc51b44e
timhbw <timhbw@gmail.com>
2020-12-17 17:25:17 +0800

    update

---
+++ src/Xmly/Auth.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Xmly;
+
+final class Auth
+{
+    private $appKey;
+    private $appSecret;
+
+    public function __construct($appKey, $appSecret)
+    {
+        $this->appKey = $appKey;
+        $this->appSecret = $appSecret;
+    }
+
+    public function getAppKey()
+    {
+        return $this->appKey;
+    }
+}
---
+++ src/Xmly/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Xmly;
+
+final class Config
+{
+    const SDK_VER = '1.0.0';
+
+    const API_HOST = 'api.ximalaya.com';
+}
