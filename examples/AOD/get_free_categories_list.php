c7e285801f851d1ca3bdd21a57da8a66aaa8d6d2
timhbw <timhbw@gmail.com>
2020-12-18 18:18:24 +0800

    add demo

---
+++ examples/AOD/get_free_categories_list.php
@@ -0,0 +1,36 @@
+<?php
+require_once __DIR__ . '/../../autoload.php';
+
+use Xmly\Auth;
+use Xmly\Config;
+use Xmly\API\AodManager;
+use Xmly\Util;
+
+$appKey = '99b37417e1185eda1378600593b45c40';
+$appSecret = 'dd7a46b12fe8a304ef17892c89ede22a';
+$serverAuthStaticKey = 'XEbin4wC';
+$auth = new Auth($appKey, $appSecret);
+
+$config = new Config();
+$config->useHTTPS = true; // 接口是否使用 HTTPS 协议
+
+$aodManager = new AodManager($auth, $config);
+
+$body = array(
+    'app_key' => $appKey,
+    'client_os_type' => 4,
+    'nonce' => Util::randomString(11),
+    'timestamp' => Util::msecTime(),
+    'device_id' => '2dfde78d016947e2982c734930951d55',
+    'server_api_version' => '1.0.0'
+);
+
+// 免费内容-分类列表：https://open.ximalaya.com/doc/detailApi?categoryId=10&articleId=6#%E5%88%86%E7%B1%BB%E5%88%97%E8%A1%A8
+
+list($ret, $err) = $aodManager->getCategoriesList($body, $serverAuthStaticKey);
+if ($err !== null) {
+    var_dump($err);
+} else {
+    var_dump($ret);
+}
+
