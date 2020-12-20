e701206a21c16c9d7a016d861583d655bde594f8
timhbw <timhbw@gmail.com>
2020-12-20 19:41:44 +0800

    update demo

--- examples/http_post_x-www-form-urlencoded.php
+++ examples/http_post_x-www-form-urlencoded.php
@@ -10,7 +10,17 @@ $auth = new Auth($appKey, $appSecret);
 
 $url = 'https://api.ximalaya.com/oauth2/v2/access_token';
 
-$req = array();
+//$req = array(
+//    'client_id' => $appKey,
+//    'client_secret' => $appSecret,
+//    'device_id' => 'xxxx',
+//    'code' => 'xxxx',
+//    'device_id' => '2dfde78d016947e2982c734930951d55',
+//    'grant_type' => 'authorization_code',
+//    'redirect_uri' => 'https://xxxx.xxxx.com/oauth2/get_access_token',
+//    'state' => 'xxxx',
+//);
+
 $req['client_id'] = $appKey;
 $req['client_secret'] = $appSecret;
 $req['device_id'] = 'xxxx';
@@ -20,11 +30,7 @@ $req['redirect_uri'] = 'https://xxxx.xxxx.com/oauth2/get_access_token';
 $req['state'] = 'xxxx';
 $body = http_build_query($req);
 
-$contentType = "application/x-www-form-urlencoded; charset=UTF-8";
-
-//$headers = $auth->getAccessToken();
-
-$headers['Content-Type'] = $contentType;
+$headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
 $response = Client::post($url, $body, $headers);
 var_dump($body);
 if ($response->ok()) {
