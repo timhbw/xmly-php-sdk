1fe428b4952b3b4e3ad9156e8ddb1543c829657a
timhbw <timhbw@gmail.com>
2020-12-20 20:21:16 +0800

    OAuthManager get device_id replace by $this->auth->getdeviceID()

--- src/Xmly/OAuth2/OAuth2Manager.php
+++ src/Xmly/OAuth2/OAuth2Manager.php
@@ -25,7 +25,6 @@ final class OAuth2Manager
     /**
      * 获取访问令牌
      *
-     * @param string $device_id
      * @param string $code
      * @param string $redirect_uri
      * @param string $state
@@ -34,12 +33,12 @@ final class OAuth2Manager
      *
      * @link https://open.ximalaya.com/doc/detailApi?categoryId=9&articleId=5#%E8%8E%B7%E5%8F%96%E8%AE%BF%E9%97%AE%E4%BB%A4%E7%89%8C
      */
-    public function getAccessToken($device_id, $code, $redirect_uri, $state, $grant_type = 'authorization_code')
+    public function getAccessToken($code, $redirect_uri, $state, $grant_type = 'authorization_code')
     {
         $params = array(
             'client_id' => $this->auth->getAppKey(),
             'client_secret' => $this->auth->getAppSecret(),
-            'device_id' => $device_id,
+            'device_id' => $this->auth->getdeviceID(),
             'code' => $code,
             'redirect_uri' => $redirect_uri,
             'state' => $state,
@@ -65,7 +64,6 @@ final class OAuth2Manager
     /**
      * 刷新访问令牌
      *
-     * @param string $device_id 设备ID
      * @param string $refresh_token 刷新令牌参数，从 /oauth2/v2/access_token 返回
      * @param string $redirect_uri OAuth2回调地址，即创建应用时填写的回调地址。使用时请对它的值进行URL编码处理，并最好提供https地址
      * @param string $grant_type 授权模式，固定值"refresh_token"
@@ -73,12 +71,12 @@ final class OAuth2Manager
      *
      * @link https://open.ximalaya.com/doc/detailApi?categoryId=9&articleId=5#%E5%88%B7%E6%96%B0%E8%AE%BF%E9%97%AE%E4%BB%A4%E7%89%8C
      */
-    public function refreshAccessToken($device_id, $refresh_token, $redirect_uri, $grant_type = 'refresh_token')
+    public function refreshAccessToken($refresh_token, $redirect_uri, $grant_type = 'refresh_token')
     {
         $params = array(
             'client_id' => $this->auth->getAppKey(),
             'client_secret' => $this->auth->getAppSecret(),
-            'device_id' => $device_id,
+            'device_id' => $this->auth->getdeviceID(),
             'refresh_token' => $refresh_token,
             'redirect_uri' => $redirect_uri,
             'grant_type' => $grant_type
@@ -103,18 +101,17 @@ final class OAuth2Manager
     /**
      * 查询访问令牌
      *
-     * @param string $device_id 设备ID
      * @param string $access_token 访问令牌
      * @return array
      *
      * @link https://open.ximalaya.com/doc/detailApi?categoryId=9&articleId=5#%E6%9F%A5%E8%AF%A2%E8%AE%BF%E9%97%AE%E4%BB%A4%E7%89%8C
      */
-    public function getAccessTokenInfo($device_id, $access_token)
+    public function getAccessTokenInfo($access_token)
     {
         $params = array(
             'client_id' => $this->auth->getAppKey(),
             'client_secret' => $this->auth->getAppSecret(),
-            'device_id' => $device_id,
+            'device_id' => $this->auth->getdeviceID(),
             'access_token' => $access_token
         );
         $data = http_build_query($params);
